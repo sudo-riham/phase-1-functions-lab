ba06f1bbba3751de98b26b93e9c2d2672808a991
rihamzeidan <r.zeidan@eweev.com>
2022-12-11 20:32:07 +0200

    Solution

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = (endBlock, startBlock=42) => Math.abs(endBlock - startBlock);
+
+const distanceFromHqInFeet = endBlock => distanceFromHqInBlocks(endBlock) * 264;
+
+const distanceTravelledInFeet = (endBlock, startBlock) => distanceFromHqInBlocks(endBlock, startBlock) * 264
+
+const calculatesFarePrice = (endBlock, startBlock) => {
+    let distance = distanceTravelledInFeet(startBlock, endBlock);
+    let cost = 0
+
+    distance = distance - 400
+
+    if (distance <= 0) {
+        return 0
+    } else {
+        while (distance > 0 && cost < 32) {
+            distance--
+            cost = cost + .02
+        }
+    
+        if (distance === 0){
+            return Math.round(cost * 100) / 100
+        }
+        else if (distance <= 500) {
+            return 25
+        } else {
+            return 'cannot travel that far'
+        }
+    }
+
+}
